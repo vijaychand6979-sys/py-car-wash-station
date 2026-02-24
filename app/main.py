2e35f997919eb42303279aa0ec49f7939e51f1be
Vijay Chand <vijaychand6979@googlemail.com>
2026-02-24 14:44:33 +0000

    Corrected rounding issue

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ class CarWashStation:
             if car.clean_mark < self.clean_power:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
-        return income
+        return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         clean_req = self.clean_power - car.clean_mark
